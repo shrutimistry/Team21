c2df93dccacb0734e8c2ca73ec753588be7e6edf
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2018-10-17 01:13:25 -0400

    Add 'Hello world' JavaFX user interface as base for UI development

---
+++ immigration-analytics/src/main/java/com/nineplusten/app/App.java
@@ -0,0 +1,45 @@
+package com.nineplusten.app;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application {
+  private Stage primaryStage;
+  private BorderPane rootLayout;
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    this.primaryStage = primaryStage;
+    this.primaryStage.setTitle("Immigration Service Analytics");
+
+    initRootLayout();
+
+  }
+
+  public void initRootLayout() {
+    try {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(App.class.getResource("view/RootLayout.fxml"));
+      rootLayout = (BorderPane) loader.load();
+
+      Scene scene = new Scene(rootLayout);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}

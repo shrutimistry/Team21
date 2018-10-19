36a920e096fd35a630224a8f54036e7c3167bcd3
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2018-10-19 16:08:17 -0400

    Implement frontend for login function

--- immigration-analytics/src/main/java/com/nineplusten/app/App.java
+++ immigration-analytics/src/main/java/com/nineplusten/app/App.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -25,6 +26,8 @@ public class App extends Application {
     this.primaryStage.setTitle("Immigration Service Analytics");
 
     initRootLayout();
+    
+    showLoginView();
 
   }
 
@@ -38,6 +41,18 @@ public class App extends Application {
       primaryStage.setScene(scene);
       primaryStage.show();
 
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void showLoginView() {
+    try {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(App.class.getResource("view/LoginView.fxml"));
+      AnchorPane loginView = (AnchorPane) loader.load();
+
+      rootLayout.setCenter(loginView);
     } catch (IOException e) {
       e.printStackTrace();
     }

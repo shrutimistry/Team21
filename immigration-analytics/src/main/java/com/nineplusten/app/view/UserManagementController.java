6ac45df52588a92626dff93a1478bd5dd59aab8f
Raya Farhadi <raya.farhadi@gmail.com>
2018-10-27 10:56:11 -0400

    created empty account management tab

---
+++ immigration-analytics/src/main/java/com/nineplusten/app/view/UserManagementController.java
@@ -0,0 +1,16 @@
+package com.nineplusten.app.view;
+
+import com.nineplusten.app.App;
+import javafx.fxml.FXML;
+
+public class UserManagementController {
+  
+  @FXML
+  private void initialize() {
+  }
+  
+  public void setMainApp(App mainApp) {
+  }
+  
+  
+}

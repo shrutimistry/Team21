ae0a3a5360db18ed4add4b67fa3e3e41c30dfc4a
Raya Farhadi <raya.farhadi@gmail.com>
2018-11-08 02:09:39 -0500

    finished service and table

--- immigration-analytics/src/main/java/com/nineplusten/app/service/LoadUserService.java
+++ immigration-analytics/src/main/java/com/nineplusten/app/service/LoadUserService.java
@@ -1,25 +1,57 @@
 package com.nineplusten.app.service;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.nineplusten.app.util.RestDbIO;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 
-public class LoadUserService extends Service<Void> {
+public class LoadUserService extends Service<List<String[]>> {
 
     @Override
-    protected Task<Void> createTask() {
-        return new Task<Void>() {
+    protected Task<List<String[]>> createTask() {
+        return new Task<List<String[]>>() {
             @Override
-            protected Void call() throws Exception {
+            protected List<String[]> call() throws Exception {
               // loads a user from the database
-              loadUserInstance();
-              return null;
+              return loadUser();
             }
         };
     }
 
-   public void loadUserInstance(){
+   public List<String[]> loadUser(){
+    JSONArray userArray = null;
+        try {
+            userArray = RestDbIO.get("/users");
+        } catch (UnirestException e){
+            e.printStackTrace();
+        }
 
+        return parseUserArray(userArray);
+    }
+
+    private List<String[]> parseUserArray(JSONArray jsonArray){
+
+        List<String[]> users = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++){
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String[] temp = new String[3];
+            temp[0] = jsonObject.optString("user_id");
+            temp[1] = jsonObject.optString("email");
+            temp[2] = jsonObject.optString("user_role");
+
+            users.add(temp);
+        }
+
+        return users;
     }
 
 }
--- immigration-analytics/src/main/java/com/nineplusten/app/view/UserManagementController.java
+++ immigration-analytics/src/main/java/com/nineplusten/app/view/UserManagementController.java
@@ -1,17 +1,28 @@
 package com.nineplusten.app.view;
 
 import com.nineplusten.app.App;
+import com.nineplusten.app.service.LoadUserService;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
+
+import java.util.List;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 class User {
 
   StringProperty userID;
   StringProperty email;
   StringProperty role;
-
+  
   public User(String userID, String email, String role){
     userIDProperty().setValue(userID);
     emailProperty().setValue(userID);
@@ -44,14 +55,64 @@ class User {
 public class UserManagementController {
 
   TableView<User> userTable;
+  LoadUserService userService;
 
   @FXML
   private void initialize() {
     
-    userTable = new TableView<User>();
+    userService = new LoadUserService();
+    userService.start();
+    userService.setOnSucceeded(new EventHandler<WorkerStateEvent>(){
+      @Override 
+      public void handle(WorkerStateEvent e) { 
+        initializeTable();
+      } 
+    });
+    
   }
 
   public void setMainApp(App mainApp) {
+  }
+
+  private ObservableList<User> parseUsers(){
+    List<String[]> userData = userService.getValue();
+    
+    List<User> userList = new ArrayList<>();
+
+    for (int i = 0; i < userData.size(); i++){
+        String[] tempArray = userData.get(i);
+        User tempUser = new User(tempArray[0], tempArray[1], tempArray[2]);
+        userList.add(tempUser);
+    }
+
+   
+    return toObservableList(userList);
+  }
+
+  private ObservableList<User> toObservableList(List<User> list){
+    ObservableList<User> observableList = FXCollections.observableArrayList();
+
+    for (int i = 0; i < list.size(); i++){
+      observableList.add(list.get(i));
+    }
+
+    return observableList;
+  }
+
+  private void initializeTable(){
+    userTable = new TableView<User>();
+
+    userTable.setItems(parseUsers());
+
+    TableColumn<User, String> userIDColumn = new TableColumn<>("User ID");
+    TableColumn<User, String> emailColumn = new TableColumn<>("Email");
+    TableColumn<User, String> roleColumn = new TableColumn<>("Role");
+
+    userIDColumn.setCellValueFactory(new PropertyValueFactory("userID"));
+    emailColumn.setCellValueFactory(new PropertyValueFactory("email"));
+    roleColumn.setCellValueFactory(new PropertyValueFactory("role"));
+
+    userTable.getColumns().setAll(userIDColumn, emailColumn, roleColumn);
   }
 
 }

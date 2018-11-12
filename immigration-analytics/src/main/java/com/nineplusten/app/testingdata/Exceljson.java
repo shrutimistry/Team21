2e5613cf3c447d83d8af53158f56354f8c45c3f1
shrutimistry <shrutivmistry@gmail.com>
2018-11-11 19:23:55 -0500

    remove json  building and add data to a hashmap

--- immigration-analytics/src/main/java/com/nineplusten/app/testingdata/Exceljson.java
+++ immigration-analytics/src/main/java/com/nineplusten/app/testingdata/Exceljson.java
@@ -2,6 +2,7 @@ package com.nineplusten.app.testingdata;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.usermodel.CellType;
@@ -12,40 +13,31 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+
+
+
 public class Exceljson {
-	JsonArray getExcelasJson(XSSFSheet sheet) {
-  	  JsonObject sheetsJsonObject = new JsonObject();
+
+	private Map<String, String> fieldData;
+	public JsonArray getExcelasJson(XSSFSheet sheet) {
   	  
   	  JsonArray sheetArray = new JsonArray();
-        ArrayList<String> columnNames = new ArrayList<String>();
-        Iterator<Row> sheetIterator = sheet.iterator();
-        
-        while (sheetIterator.hasNext()) {
-
-            Row currentRow = sheetIterator.next();
-            JsonObject jsonObject = new JsonObject();
-            if (currentRow.getRowNum() != 0) {
-          	  for (int j = 0; j < columnNames.size(); j++) {
+      ArrayList<String> columnNames = new ArrayList<String>();
+      Iterator<Row> sheetIterator = sheet.iterator();
+      while (sheetIterator.hasNext()) {
+    	  Row currentRow = sheetIterator.next();
+           JsonObject jsonObject = new JsonObject();
+           if (currentRow.getRowNum() >= 3) {
+          	 for (int j = 0; j < columnNames.size(); j++) {
           		  if (currentRow.getCell(j) != null) {
           			  	System.out.println(currentRow.getCell(j));
-                        if (currentRow.getCell(j).getCellTypeEnum() == CellType.STRING) {
-                            jsonObject.addProperty(columnNames.get(j), currentRow.getCell(j).getStringCellValue());
-                        } else if (currentRow.getCell(j).getCellTypeEnum() == CellType.NUMERIC) {
-                            jsonObject.addProperty(columnNames.get(j), currentRow.getCell(j).getNumericCellValue());
-                        } else if (currentRow.getCell(j).getCellTypeEnum() == CellType.BOOLEAN) {
-                            jsonObject.addProperty(columnNames.get(j), currentRow.getCell(j).getBooleanCellValue());
-                        } else if (currentRow.getCell(j).getCellTypeEnum() == CellType.BLANK) {
-                            jsonObject.addProperty(columnNames.get(j), "");
-                        }
+          			  	String value = currentRow.getCell(j).toString();
+          			  	System.out.println(columnNames.get(j));
+          			  	fieldData.put(columnNames.get(j), value);
                     } else {
-                        jsonObject.addProperty(columnNames.get(j), "");
+                        fieldData.put(columnNames.get(j), "");
                     }
           	  }
-            } else {
-                // store column names
-                for (int k = 0; k < currentRow.getPhysicalNumberOfCells(); k++) {
-                    columnNames.add(currentRow.getCell(k).getStringCellValue());
-                }
             }
         }
 		return sheetArray;
